e519a9cba5b3a8c47706b9fce7f6d993511d7265
K.M. SAZZADUL ISLAM <18-37693-1@student.aiub.edu>
2022-10-11 03:03:57 +0600

    Nav & banner done

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../img/logo.jpg'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Link to={'/home'}>Home</Link>
-            <p>aklfaklfjkladj</p>
-        </div>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container justify-content-around">
+                <img className='logo navbar-brand' src={logo} alt="" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to={'/home'}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={'/stat'}>Statistic</Link>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        // <div className='d-flex justify-content-around align-items-center'>
+        //     
+
+        //     <div>
+        //         
+
+        //         
+        //     </div>
+            
+        // </div>
     );
 };
 
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import backgroud from '../../img/banner.jpg'
+import './Home.css'
+
 
 const Home = () => {
     return (
         <div>
-            <p>lkfjklafjklajfdlkasjfkld</p>
+            <div className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner text-center">
+                    <div className="carousel-item d-flex flex-column h-100 align-items-center justify-content-center text-white  active">
+                        <h1 className='py-5'>Select Your Favourite Quiz Topic</h1>
+
+                        <h6 className=''>Topics: HTML, CSS, JavaScript, React, Node JS, Problem Solving</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                
+            </div>
         </div>
     );
 };
